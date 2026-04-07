cf0a736ae2464252361537c802697dbab09fa6b5
Gabriel Souza <gabrielhalls77@gmail.com>
2026-04-07 20:17:02 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const apiUrl = process.env.API_INTERNAL_URL || "http://external-projects_dermahu
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  turbopack: {
+    root: ".",
+  },
   async rewrites() {
     return [
       {
